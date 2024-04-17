f18db4445f59e4cc7e185e9d26e636882a1ecc28
Echedey Henríquez <echedeyhh1989@gmail.com>
2024-04-17 18:51:06 +0100

    Añadimos el primer commit

---
+++ src/figuras/Circunferencia.java
@@ -0,0 +1,44 @@
+package figuras;
+
+public class Circunferencia {
+	private double radio;
+	private String color;
+	private double d;
+
+	public Circunferencia(double radio) {
+		this.radio = radio;
+	}
+
+	public void imprimir() {
+		color = "rojo";
+		System.out.println("Diámetro: " + 2 * radio);
+		System.out.println("Color: " + color);
+		double d = 2 * 3.1416 * radio * radio;
+		System.out.println(d);
+	}
+
+
+	public double getRad() {
+		return radio;
+	}
+
+	public void setRad(double rad) {
+		this.radio = rad;
+	}
+
+	public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
+		double radio1 = this.radio;
+		double radio2 = otro.getRad();
+		if (considerarDecimales) {
+			if (radio1 == radio2)
+				return true;
+			else
+				return false;
+		} else {
+			if (Math.abs(radio1 - radio2) < 1)
+				return true;
+			else
+				return false;
+		}
+	}
+}
